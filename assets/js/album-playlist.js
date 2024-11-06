23f1b6773fde1275989e613db0d888ebe7cb2436
ban <summer.jyyy@gmail.com>
2024-11-06 19:42:23 +0900

    feat: play 버튼 기능 구현

--- assets/js/album-playlist.js
+++ assets/js/album-playlist.js
@@ -1,13 +1,19 @@
 const playBtn = document.querySelector('#playAlbum');
 const cdWrap = document.querySelectorAll('.cd-wrap>img');
 const cdArr = Array.from(cdWrap);
+const pausedImg = 'assets/images/albums/paused-circle.png';
+const playImg = "assets/images/albums/play-circle.png";
 
-//현재 선택된 앨범 선언
+//현재 앨범과 앨범인덱스 선언
 let nowAlbum = "";
+let nowAlbumIndex = "";
+
+//hidden 여부에따라 현재 보여지는 앨범을 선택
 function nowAlbumSelect(){
   for(let i = 0; i < cdArr.length; i++){
     if(!(cdArr[i].classList.contains("hidden"))){
       nowAlbum = cdArr[i];
+      nowAlbumIndex = i;
     }
   }
 }
@@ -19,17 +25,55 @@ function nowAlbumSelect(){
  * animation-play-state: running / paused
  */
 function playAlbum() {
-  //현재 보여지는 앨범 선택
   nowAlbumSelect();
 
-  //노래 재생
   if (nowAlbum.classList.contains('running') && !nowAlbum.classList.contains('paused')) {
     //재생중일때, 정지
     nowAlbum.classList.add('paused')
+    playBtn.querySelector('img').src = pausedImg;
   } else if (nowAlbum.classList.contains('paused')) {
     //일시정지일때, 일시정지 삭제
     nowAlbum.classList.remove('paused')
+    playBtn.querySelector('img').src = playImg;
   } else if (!nowAlbum.classList.contains('running')) {
+    //재생을 안했을때, 재생
     nowAlbum.classList.add('running')
+    playBtn.querySelector('img').src = playImg;
   }
+}
+
+//nextBtn, preBtn 클릭 -> hidden 클래스 변경, nowAlbum 선택
+function toNextAlbum(){
+  nowAlbum.classList.remove("running");
+  if(nowAlbumIndex == (cdArr.length-1)){
+    cdArr[cdArr.length-1].classList.add("hidden");
+    cdArr[0].classList.remove("hidden");
+  }
+  else{
+    for(let i = 0; i< cdArr.length; i++){
+      if(i == nowAlbumIndex){
+        cdArr[i].classList.add("hidden");
+      }
+      if(i == (nowAlbumIndex+1)){
+        cdArr[i].classList.remove("hidden");
+      }
+  }}
+  nowAlbumSelect();
+}
+function toPreAlbum(){
+  nowAlbum.classList.remove("running");
+  if (nowAlbumIndex == 0) {
+    cdArr[0].classList.add('hidden');
+    cdArr[cdArr.length-1].classList.remove('hidden');
+  } else {
+    for (let i = 0; i < cdArr.length; i++) {
+      if (i == nowAlbumIndex) {
+        cdArr[i].classList.add('hidden');
+      }
+      if (i == (nowAlbumIndex-1)) {
+        cdArr[i].classList.remove('hidden');
+      }
+    }
+  }
+  nowAlbumSelect();
 }
