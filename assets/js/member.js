07cb540acdbadd138cfdb9dadc2bd54a68ac0ef4
김혜빈 <hyebinkimdesign@gmail.com>
2024-11-06 16:42:58 +0900

    feat: 멤버페이지 페이지네이션 선택에 따라 이미지 변경 구현

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,3 +1,10 @@
+//멤버별 페이지 호출
+const minji_images = document.querySelector('.Team-photo-minji-page')
+const hanni_images = document.querySelector('.Team-photo-hanni-page')
+const dani_images = document.querySelector('.Team-photo-dani-page')
+const haerin_images = document.querySelector('.Team-photo-haerin-page')
+const hyein_images = document.querySelector('.Team-photo-hyein-page')
+
 //페이지네이션 호출
 const pagination_btn_01 = document.querySelector('.member_Pagination01 img')
 const pagination_btn_02 = document.querySelector('.member_Pagination02 img')
@@ -12,22 +19,66 @@ pagination_btn_01.addEventListener('click', function () {
   pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
+
+  minji_images.style.display = 'block'
+  hanni_images.style.display = 'none'
+  dani_images.style.display = 'none'
+  haerin_images.style.display = 'none'
+  hyein_images.style.display = 'none'
 })
 
-//페이지네이션 민지 버튼 클릭 시
+//페이지네이션 하니 버튼 클릭 시
 pagination_btn_02.addEventListener('click', function () {
   pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_02.src = 'assets/images/member/photo-page-2.png'
   pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
+
+  minji_images.style.display = 'none'
+  hanni_images.style.display = 'block'
+  dani_images.style.display = 'none'
+  haerin_images.style.display = 'none'
+  hyein_images.style.display = 'none'
 })
+
 pagination_btn_03.addEventListener('click', function () {
   pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_03.src = 'assets/images/member/photo-page-3.png'
   pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
   pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
+  minji_images.style.display = 'none'
+  hanni_images.style.display = 'none'
+  dani_images.style.display = 'block'
+  haerin_images.style.display = 'none'
+  hyein_images.style.display = 'none'
+})
+//페이지네이션 해린 버튼 클릭 시
+pagination_btn_04.addEventListener('click', function () {
+  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_03.src = 'assets/images/member/photo-page-3.png'
+  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
+  minji_images.style.display = 'none'
+  hanni_images.style.display = 'none'
+  dani_images.style.display = 'none'
+  haerin_images.style.display = 'block'
+  hyein_images.style.display = 'none'
+})
+//페이지네이션 혜인 버튼 클릭 시
+pagination_btn_05.addEventListener('click', function () {
+  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_03.src = 'assets/images/member/photo-page-3.png'
+  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
+  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
+  minji_images.style.display = 'none'
+  hanni_images.style.display = 'none'
+  dani_images.style.display = 'none'
+  haerin_images.style.display = 'none'
+  hyein_images.style.display = 'block'
 })
 pagination_btn_04.addEventListener('click', function () {
   pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
