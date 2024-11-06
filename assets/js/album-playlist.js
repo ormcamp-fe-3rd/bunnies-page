ba5a21da16361474046111561de1530d232525a4
ban <summer.jyyy@gmail.com>
2024-11-06 16:47:15 +0900

    현재 앨범 식별기능 추가

--- assets/js/album-playlist.js
+++ assets/js/album-playlist.js
@@ -1,20 +1,35 @@
-const playBtn = document.querySelector('#playBtn')
-const cd = document.querySelector('#cd_1')
+const playBtn = document.querySelector('#playAlbum');
+const cdWrap = document.querySelectorAll('.cd-wrap>img');
+const cdArr = Array.from(cdWrap);
+
+//현재 선택된 앨범 선언
+let nowAlbum = "";
+function nowAlbumSelect(){
+  for(let i = 0; i < cdArr.length; i++){
+    if(!(cdArr[i].classList.contains("hidden"))){
+      nowAlbum = cdArr[i];
+    }
+  }
+}
 
 /**
  * cd 재생 애니메이션(회전)
- * 
+ *
  * cd 이미지의 dom class로 조절
  * animation-play-state: running / paused
  */
-function playMusic() {
-  if (cd.classList.contains('running') && !cd.classList.contains('paused')) {
+function playAlbum() {
+  //현재 보여지는 앨범 선택
+  nowAlbumSelect();
+
+  //노래 재생
+  if (nowAlbum.classList.contains('running') && !nowAlbum.classList.contains('paused')) {
     //재생중일때, 정지
-    cd.classList.add('paused')
-  } else if (cd.classList.contains('paused')) {
+    nowAlbum.classList.add('paused')
+  } else if (nowAlbum.classList.contains('paused')) {
     //일시정지일때, 일시정지 삭제
-    cd.classList.remove('paused')
-  } else if (!cd.classList.contains('running')) {
-    cd.classList.add('running')
+    nowAlbum.classList.remove('paused')
+  } else if (!nowAlbum.classList.contains('running')) {
+    nowAlbum.classList.add('running')
   }
 }
