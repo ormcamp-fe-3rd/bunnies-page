c6a4a8cc925d3c0739f300b2c8adb730ed5eae64
naseulgee <thwndglgkwk@naver.com>
2024-11-07 17:59:45 +0900

    fix: DOM 로드 시 실행 함수 통합

--- assets/js/card-rotate.js
+++ assets/js/card-rotate.js
@@ -1,18 +1,14 @@
-var cards = document.querySelectorAll('.member-card-box li')
 function rotateCards() {
+  const cards = document.querySelectorAll('.member-card-box li')
   // 카드들 선택
   for (var i = 0; i < cards.length; i++) {
-    var card = cards[i]
+    const card = cards[i]
     // -12 ~ +12 사이 무작위 숫자 선출
-    var randomNumber = Math.random()
+    let randomNumber = Math.random()
     randomNumber = randomNumber * 25
     randomNumber = Math.floor(randomNumber)
     // 각도에 무작위 숫자 적용
-    var degree = randomNumber - 12
+    const degree = randomNumber - 12
     card.style.transform = 'rotate(' + degree + 'deg)'
   }
-}
-// 페이지 로드시 카드 회전 실행
-window.onload = function () {
-  rotateCards()
 }
