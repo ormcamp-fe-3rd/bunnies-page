95aee5a9f62e691c94e139787bfb5020c217dc7c
parkHansol <hansol.woff@gmail.com>
2024-11-08 02:01:43 +0900

    [style] var -> let 으로 수정

--- assets/js/card-rotate.js
+++ assets/js/card-rotate.js
@@ -1,14 +1,14 @@
-var cards = document.querySelectorAll('.member-card-box li')
+let cards = document.querySelectorAll('.member-card-box li')
 function rotateCards() {
   // 카드들 선택
-  for (var i = 0; i < cards.length; i++) {
-    var card = cards[i]
+  for (let i = 0; i < cards.length; i++) {
+    let card = cards[i]
     // -12 ~ +12 사이 무작위 숫자 선출
-    var randomNumber = Math.random()
+    let randomNumber = Math.random()
     randomNumber = randomNumber * 25
     randomNumber = Math.floor(randomNumber)
     // 각도에 무작위 숫자 적용
-    var degree = randomNumber - 12
+    let degree = randomNumber - 12
     card.style.transform = 'rotate(' + degree + 'deg)'
   }
 }
