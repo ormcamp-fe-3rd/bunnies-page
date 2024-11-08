89dc6b540ee9407e3df47170775c6236364cbd51
ban <summer.jyyy@gmail.com>
2024-11-08 15:51:07 +0900

    design: 메시지 섹션 이미지 수정 및 누락부분 복구

--- assets/js/member.js
+++ assets/js/member.js
@@ -160,3 +160,60 @@ function runDrag(cards) {
   })
 }
 runDrag('.team-photo')
+
+/**
+ * home에서 멤버카드 선택시 member페이지로 이동
+ * @param {name} 선택된 멤버의 이름 
+ */
+function toMemberPage(name){
+  switch(name){
+    case "home-minji":
+      location.href = "/member.html"
+      sessionStorage.setItem('name', 'minji')
+      break
+    case "home-danni": 
+      location.href = '/member.html'
+      sessionStorage.setItem('name', 'danni')
+      break
+    case "home-hanni":
+      location.href = '/member.html'
+      sessionStorage.setItem("name", "hanni")
+      break
+    case "home-hyein":
+      location.href = '/member.html'
+      sessionStorage.setItem('name', 'hyein')
+      break
+    case "home-haerin":
+      location.href = '/member.html'
+      sessionStorage.setItem('name', 'haerin')
+      break
+  }
+}
+
+/**
+ * member페이지 로드시마다
+ * 세션에 저장된 이름이 있으면, 해당하는 이름의 페이지네이션 클릭
+ */
+window.addEventListener("load",()=>{
+  let clickName = sessionStorage.getItem("name")
+  switch(clickName){
+    case "minji":
+      pagination_btn_01.click()
+      break
+    case "hanni":
+      pagination_btn_02.click()
+      break
+    case "danni":
+      pagination_btn_03.click()
+      break
+    case "haerin":
+      pagination_btn_04.click()
+      break
+    case "hyein":
+      pagination_btn_05.click()
+      break
+    default:
+      pagination_btn_01.click()
+  }
+  sessionStorage.clear()
+})
