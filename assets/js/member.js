808f694214094e9548f3aa12a4b752c1bdf89511
naseulgee <thwndglgkwk@naver.com>
2024-11-09 17:41:53 +0900

    perf: 멤버 페이지 포토카드 영역 반응형 처리 및 중복 코드 함수화

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,119 +1,84 @@
-//멤버별 페이지 호출
-const minji_images = document.querySelector('.team-photo-minji-page')
-const hanni_images = document.querySelector('.team-photo-hanni-page')
-const dani_images = document.querySelector('.team-photo-dani-page')
-const haerin_images = document.querySelector('.team-photo-haerin-page')
-const hyein_images = document.querySelector('.team-photo-hyein-page')
+const pages = document.querySelectorAll('.team-photo-page')
+const pagingBtns = document.querySelectorAll('.member-pagination ul li')
 
-//페이지네이션 호출
-const pagination_btn_01 = document.querySelector('.member-pagination01 img')
-const pagination_btn_02 = document.querySelector('.member-pagination02 img')
-const pagination_btn_03 = document.querySelector('.member-pagination03 img')
-const pagination_btn_04 = document.querySelector('.member-pagination04 img')
-const pagination_btn_05 = document.querySelector('.member-pagination05 img')
-
-//페이지 네이션 기능
-//페이지네이션 민지 버튼 클릭 시
-pagination_btn_01.addEventListener('click', function () {
-  //클릭 버튼만 활성화 이미지로 변경
-  //그외 버튼들은 비활성화 이미지로 변경
-  pagination_btn_01.src = 'assets/images/member/photo-page-1.png'
-  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
-  //민지 페이지 표시
-  minji_images.style.display = 'block'
-  //그외 페이지들은 화면에서 none처리
-  hanni_images.style.display = 'none'
-  dani_images.style.display = 'none'
-  haerin_images.style.display = 'none'
-  hyein_images.style.display = 'none'
-  //드래그앤드롭 함수를 재실행시켜 y좌표 이동 제한을 설정함
-  runDrag('.team-photo')
-})
-
-//페이지네이션 하니 버튼 클릭 시
-pagination_btn_02.addEventListener('click', function () {
-  //클릭 버튼만 활성화 이미지로 변경
-  //그외 버튼들은 비활성화 이미지로 변경
-  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_02.src = 'assets/images/member/photo-page-2.png'
-  pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
-  //하니 페이지 표시 그 외 페이지는 none 처리
-  minji_images.style.display = 'none'
-  hanni_images.style.display = 'block'
-  dani_images.style.display = 'none'
-  haerin_images.style.display = 'none'
-  hyein_images.style.display = 'none'
-  //드래그앤드롭 함수를 재실행시켜 y좌표 이동 제한을 설정함
-  runDrag('.team-photo')
-})
-
-pagination_btn_03.addEventListener('click', function () {
-  //클릭 버튼만 활성화 이미지로 변경
-  //그외 버튼들은 비활성화 이미지로 변경
-  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_03.src = 'assets/images/member/photo-page-3.png'
-  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
-  //다니 페이지 표시 그 외 페이지는 none 처리
-  minji_images.style.display = 'none'
-  hanni_images.style.display = 'none'
-  dani_images.style.display = 'block'
-  haerin_images.style.display = 'none'
-  hyein_images.style.display = 'none'
-  //드래그앤드롭 함수를 재실행시켜 y좌표 이동 제한을 설정함
-  runDrag('.team-photo')
-})
-//페이지네이션 해린 버튼 클릭 시
-pagination_btn_04.addEventListener('click', function () {
-  //클릭 버튼만 활성화 이미지로 변경
-  //그외 버튼들은 비활성화 이미지로 변경
-  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_04.src = 'assets/images/member/photo-page-4.png'
-  pagination_btn_05.src = 'assets/images/member/photo-page-none.png'
-  //해린 페이지 표시 그 외 페이지는 none 처리
-  minji_images.style.display = 'none'
-  hanni_images.style.display = 'none'
-  dani_images.style.display = 'none'
-  haerin_images.style.display = 'block'
-  hyein_images.style.display = 'none'
-  //드래그앤드롭 함수를 재실행시켜 y좌표 이동 제한을 설정함
-  runDrag('.team-photo')
-})
-
-//페이지네이션 혜인 버튼 클릭 시
-pagination_btn_05.addEventListener('click', function () {
-  //클릭 버튼만 활성화 이미지로 변경
-  //그외 버튼들은 비활성화 이미지로 변경
-  pagination_btn_01.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_02.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_03.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_04.src = 'assets/images/member/photo-page-none.png'
-  pagination_btn_05.src = 'assets/images/member/photo-page-5.png'
-  //혜인 페이지 표시 그 외 페이지는 none 처리
-  minji_images.style.display = 'none'
-  hanni_images.style.display = 'none'
-  dani_images.style.display = 'none'
-  haerin_images.style.display = 'none'
-  hyein_images.style.display = 'block'
-  //드래그앤드롭 함수를 재실행시켜 y좌표 이동 제한을 설정함
-  runDrag('.team-photo')
+/**
+ * member페이지 로드시마다 호출되는 함수
+ */
+window.addEventListener('load', () => {
+  setPagingEvent()
+  setPagingByParam()
+  runDrag()
 })
 
 /**
+ * 페이징 버튼에 클릭 이벤트를 추가하는 함수
+ */
+function setPagingEvent() {
+  pagingBtns.forEach((btn, index) => {
+    btn.addEventListener('click', function () {
+      togglePage(index)
+      togglePaging(index)
+    })
+  })
+}
+
+/**
+ * 전달받은 순번에 해당하는 페이지만 활성화 처리하는 함수
+ *
+ * @param {number} index 페이지 순서
+ */
+function togglePage(index) {
+  pages.forEach(page => (page.style.display = 'none'))
+  pages[index].style.display = 'block'
+}
+
+/**
+ * 전달받은 순번에 해당하는 페이징만 활성화 처리하는 함수
+ *
+ * @param {number} index 페이징 순서
+ */
+function togglePaging(index) {
+  const pagingImg = '/assets/images/member/photo-page-'
+  pagingBtns.forEach(btn => (btn.querySelector('img').src = pagingImg + 'none.png'))
+  pagingBtns[index].querySelector('img').src = pagingImg + (index + 1) + '.png'
+}
+
+/**
+ * 세션에 저장된 이름이 있으면(메인에서 전달) 해당하는 이름의 페이지네이션으로 이동하는 함수
+ */
+function setPagingByParam() {
+  let clickName = sessionStorage.getItem('name')
+  switch (clickName) {
+    case 'minji':
+      pagingBtns[0].click()
+      break
+    case 'hanni':
+      pagingBtns[1].click()
+      break
+    case 'danni':
+      pagingBtns[2].click()
+      break
+    case 'haerin':
+      pagingBtns[3].click()
+      break
+    case 'hyein':
+      pagingBtns[4].click()
+      break
+    default:
+      pagingBtns[0].click()
+  }
+  sessionStorage.clear()
+}
+
+const photoWrap = document.getElementById('photo-wrap')
+
+/**
  * 드래그 앤 드롭 액션 함수
- * @param {string} cards 드래그되는 카드들 샐렉터 설정
+ *
  * @param {MouseEvent} e - 최종 실행되는 드래그 앤 드롭 함수
  */
-function runDrag(cards) {
-  document.querySelectorAll(cards).forEach(contentDragged => {
+function runDrag() {
+  document.querySelectorAll('.team-photo').forEach(contentDragged => {
     // 마우스 시작 좌표 선언, 이동 거리 값 0으로 설정
     let xStart,
       yStart,
@@ -122,7 +87,7 @@ function runDrag(cards) {
 
     // 카드의 초기 y 좌표를 가져와, 이동 가능한 최하단 y 좌표로 설정
     const initialYPosition = contentDragged.getBoundingClientRect().top + window.scrollY
-    const limitYPosition = 420 // 각 카드가 도달할 수 있는 최하단 y 좌표
+    const limitYPosition = photoWrap.offsetTop + photoWrap.clientHeight - contentDragged.clientHeight // 각 카드가 도달할 수 있는 최하단 y 좌표
 
     /**
      * 마우스 이동 거리 계산 함수
@@ -159,61 +124,3 @@ function runDrag(cards) {
     })
   })
 }
-runDrag('.team-photo')
-
-/**
- * home에서 멤버카드 선택시 member페이지로 이동
- * @param {name} 선택된 멤버의 이름
- */
-function toMemberPage(name) {
-  switch (name) {
-    case 'home-minji':
-      location.href = '/member.html'
-      sessionStorage.setItem('name', 'minji')
-      break
-    case 'home-danni':
-      location.href = '/member.html'
-      sessionStorage.setItem('name', 'danni')
-      break
-    case 'home-hanni':
-      location.href = '/member.html'
-      sessionStorage.setItem('name', 'hanni')
-      break
-    case 'home-hyein':
-      location.href = '/member.html'
-      sessionStorage.setItem('name', 'hyein')
-      break
-    case 'home-haerin':
-      location.href = '/member.html'
-      sessionStorage.setItem('name', 'haerin')
-      break
-  }
-}
-
-/**
- * member페이지 로드시마다
- * 세션에 저장된 이름이 있으면(메인에서 전달) 해당하는 이름의 페이지네이션으로
- */
-window.addEventListener('load', () => {
-  let clickName = sessionStorage.getItem('name')
-  switch (clickName) {
-    case 'minji':
-      pagination_btn_01.click()
-      break
-    case 'hanni':
-      pagination_btn_02.click()
-      break
-    case 'danni':
-      pagination_btn_03.click()
-      break
-    case 'haerin':
-      pagination_btn_04.click()
-      break
-    case 'hyein':
-      pagination_btn_05.click()
-      break
-    default:
-      pagination_btn_01.click()
-  }
-  sessionStorage.clear()
-})
