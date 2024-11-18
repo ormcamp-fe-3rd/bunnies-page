f84c2c3c2edd801a7d7e28de5be309dca5c1070e
incolore9 <hansol.woff@gmail.com>
2024-11-18 15:07:49 +0900

    rename: js 병합 후 파일명 변경
    Co-authored-by: baaanjy <summer.jyyy@gmail.com>

--- assets/js/card & modal.js
+++ assets/js/card & modal.js
@@ -1,3 +1,21 @@
+/**
+ * 카드 회전 각도를 랜덤하게 세팅하는 함수
+ */
+function rotateCards() {
+  const cards = document.querySelectorAll('.member-card-box li')
+  // 카드들 선택
+  for (let i = 0; i < cards.length; i++) {
+    let card = cards[i]
+    // -12 ~ +12 사이 무작위 숫자 선출
+    let randomNumber = Math.random()
+    randomNumber = randomNumber * 25
+    randomNumber = Math.floor(randomNumber)
+    // 각도에 무작위 숫자 적용
+    let degree = randomNumber - 12
+    card.style.transform = 'rotate(' + degree + 'deg)'
+  }
+}
+
 const albumList = [
   {
     name: 'New Jeans',
@@ -249,22 +267,4 @@ function setIconActive(index) {
   const iconList = document.querySelectorAll('#album-icon-warp button')
   iconList.forEach(icon => (icon.style.backgroundColor = ''))
   if (index != null) iconList[index].style.backgroundColor = 'rgb(255 255 255 / var(--tw-bg-opacity))'
-}
-
-/**
- * 카드 회전 각도를 랜덤하게 세팅하는 함수
- */
-function rotateCards() {
-  const cards = document.querySelectorAll('.member-card-box li')
-  // 카드들 선택
-  for (let i = 0; i < cards.length; i++) {
-    let card = cards[i]
-    // -12 ~ +12 사이 무작위 숫자 선출
-    let randomNumber = Math.random()
-    randomNumber = randomNumber * 25
-    randomNumber = Math.floor(randomNumber)
-    // 각도에 무작위 숫자 적용
-    let degree = randomNumber - 12
-    card.style.transform = 'rotate(' + degree + 'deg)'
-  }
 }
